e7f4a8dd3371e67d3fed3524ab30f070b2aedaa9
Yevgeniy <yevgeniy.engineer@gmail.com>
2023-11-13 23:27:35 +0800

    More advanced implementation

--- examples/stack_vector.cpp
+++ examples/stack_vector.cpp
@@ -6,10 +6,6 @@
 class ResourceManagingType
 {
   public:
-    ResourceManagingType() : data_(nullptr)
-    {
-    }
-
     ResourceManagingType(const char *text)
     {
         data_ = new char[std::strlen(text) + 1];
@@ -71,19 +67,59 @@ int main()
 {
     containers::StackVector<ResourceManagingType, 5> stack_vector{};
 
+    // Adding elements
     stack_vector.emplace_back("Hello");
     stack_vector.emplace_back("World");
+    stack_vector.emplace_back("StackVector");
 
-    std::cout << "Current elements: ";
+    // Forward iteration
+    std::cout << "Forward iteration: ";
     for (const auto &element : stack_vector)
     {
         std::cout << element.getData() << " ";
     }
     std::cout << std::endl;
 
-    stack_vector.pop_back();
+    // Reverse iteration
+    std::cout << "Reverse iteration: ";
+    for (auto rit = stack_vector.rbegin(); rit != stack_vector.rend(); ++rit)
+    {
+        std::cout << (*rit).getData() << " ";
+    }
+    std::cout << std::endl;
 
+    // Random index access
+    std::cout << "Element at index 1: " << stack_vector[1].getData() << std::endl;
+
+    // Using 'at' for safe index access
+    try
+    {
+        std::cout << "Element at index 2: " << stack_vector.at(2).getData() << std::endl;
+    }
+    catch (const std::out_of_range &e)
+    {
+        std::cout << "Exception: " << e.what() << std::endl;
+    }
+
+    // Removing elements
+    stack_vector.pop_back();
     std::cout << "After pop_back: ";
+    for (const auto &element : stack_vector)
+    {
+        std::cout << element.getData() << " ";
+    }
+    std::cout << std::endl;
+
+    // Accessing front and back elements
+    if (!stack_vector.empty())
+    {
+        std::cout << "First element: " << stack_vector.front().getData() << std::endl;
+        std::cout << "Last element: " << stack_vector.back().getData() << std::endl;
+    }
+
+    // Resize the vector
+    stack_vector.resize(2, ResourceManagingType("Resize"));
+    std::cout << "After resizing to 2: ";
     for (const auto &element : stack_vector)
     {
         std::cout << element.getData() << " ";
--- include/stack_vector.hpp
+++ include/stack_vector.hpp
@@ -5,6 +5,8 @@
 #include <array>
 #include <cstddef>
 #include <cstdint>
+#include <iterator>
+#include <new>
 #include <stdexcept>
 #include <type_traits>
 #include <utility>
@@ -21,10 +23,19 @@ template <typename T, std::size_t MaxSize> class StackVector final
     {
     }
 
+    // Destructor
+    ~StackVector()
+    {
+        clear();
+    }
+
     // Copy constructor
     StackVector(const StackVector &other) : size_{other.size_}
     {
-        std::copy(other.data_.begin(), other.data_.begin() + size_, data_.begin());
+        for (std::size_t i = 0; i < size_; ++i)
+        {
+            new (&data_[i * sizeof(T)]) T{other[i]};
+        }
     }
 
     // Copy assignment operator
@@ -32,8 +43,15 @@ template <typename T, std::size_t MaxSize> class StackVector final
     {
         if (this != &other)
         {
+            // Destruct existing elements
+            clear();
+
+            // Copy construct new elements
+            for (std::size_t i = 0; i < other.size_; ++i)
+            {
+                new (&data_[i * sizeof(T)]) T{other[i]};
+            }
             size_ = other.size_;
-            std::copy(other.data_.begin(), other.data_.begin() + size_, data_.begin());
         }
 
         return *this;
@@ -42,7 +60,10 @@ template <typename T, std::size_t MaxSize> class StackVector final
     // Move constructor
     StackVector(StackVector &&other) noexcept : size_{other.size_}
     {
-        std::move(other.data_.begin(), other.data_.begin() + size_, data_.begin());
+        for (std::size_t i = 0; i < size_; ++i)
+        {
+            new (&data_[i * sizeof(T)]) T{std::move(other[i])};
+        }
         other.size_ = 0U;
     }
 
@@ -51,12 +72,292 @@ template <typename T, std::size_t MaxSize> class StackVector final
     {
         if (this != &other)
         {
+            // Destruct existing elements
+            clear();
+
+            // Move construct new elements
+            for (std::size_t i = 0; i < other.size_; ++i)
+            {
+                new (&data_[i * sizeof(T)]) T{std::move(other[i])};
+            }
             size_ = other.size_;
-            std::move(other.data_.begin(), other.data_.begin() + size_, data_.begin());
             other.size_ = 0U;
         }
 
         return *this;
+    }
+
+    void swap(StackVector &other) noexcept
+    {
+        // Swap sizes
+        std::swap(size_, other.size_);
+
+        // Swap contents
+        for (std::size_t i = 0U; i < std::max(size_, other.size_); ++i)
+        {
+            std::swap(data_[i], other.data_[i]);
+        }
+    }
+
+    class iterator final
+    {
+      public:
+        using iterator_category = std::random_access_iterator_tag;
+        using value_type = T;
+        using difference_type = std::ptrdiff_t;
+        using pointer = T *;
+        using reference = T &;
+
+        iterator(pointer ptr) noexcept : ptr_{ptr}
+        {
+        }
+
+        inline reference operator*() const noexcept
+        {
+            return *ptr_;
+        }
+        inline pointer operator->() noexcept
+        {
+            return ptr_;
+        }
+
+        // Arithmetic operations
+        inline iterator &operator++() noexcept
+        {
+            ptr_++;
+            return *this;
+        }
+        inline iterator operator++(int) noexcept
+        {
+            iterator temp = *this;
+            ++ptr_;
+            return temp;
+        }
+        inline iterator &operator--() noexcept
+        {
+            ptr_--;
+            return *this;
+        }
+        inline iterator operator--(int) noexcept
+        {
+            iterator temp = *this;
+            --ptr_;
+            return temp;
+        }
+        inline iterator &operator+=(const difference_type n) noexcept
+        {
+            ptr_ += n;
+            return *this;
+        }
+        inline iterator &operator-=(const difference_type n) noexcept
+        {
+            ptr_ -= n;
+            return *this;
+        }
+        inline iterator operator+(const difference_type n) const noexcept
+        {
+            return iterator{ptr_ + n};
+        }
+        inline iterator operator-(const difference_type n) const noexcept
+        {
+            return iterator{ptr_ - n};
+        }
+        inline difference_type operator-(const iterator &other) const noexcept
+        {
+            return (ptr_ - other.ptr_);
+        }
+
+        // Comparison operators
+        inline friend bool operator==(const iterator &a, const iterator &b) noexcept
+        {
+            return (a.ptr_ == b.ptr_);
+        }
+        inline friend bool operator!=(const iterator &a, const iterator &b) noexcept
+        {
+            return (a.ptr_ != b.ptr_);
+        }
+        inline friend bool operator<(const iterator &a, const iterator &b) noexcept
+        {
+            return (a.ptr_ < b.ptr_);
+        }
+        inline friend bool operator>(const iterator &a, const iterator &b) noexcept
+        {
+            return (a.ptr_ > b.ptr_);
+        }
+        inline friend bool operator<=(const iterator &a, const iterator &b) noexcept
+        {
+            return (a.ptr_ <= b.ptr_);
+        }
+        inline friend bool operator>=(const iterator &a, const iterator &b) noexcept
+        {
+            return (a.ptr_ >= b.ptr_);
+        }
+
+        // Subscript operator
+        inline reference operator[](const difference_type n) const noexcept
+        {
+            return *(ptr_ + n);
+        }
+
+      private:
+        pointer ptr_;
+    };
+
+    class const_iterator final
+    {
+      public:
+        using iterator_category = std::random_access_iterator_tag;
+        using value_type = const T;
+        using difference_type = std::ptrdiff_t;
+        using pointer = const T *;
+        using reference = const T &;
+
+        const_iterator(pointer ptr) noexcept : ptr_{ptr}
+        {
+        }
+
+        inline reference operator*() const noexcept
+        {
+            return *ptr_;
+        }
+        inline pointer operator->() const noexcept
+        {
+            return ptr_;
+        }
+
+        // Arithmetic operations
+        inline const_iterator &operator++() noexcept
+        {
+            ptr_++;
+            return *this;
+        }
+        inline const_iterator operator++(int) noexcept
+        {
+            const_iterator temp = *this;
+            ++ptr_;
+            return temp;
+        }
+        inline const_iterator &operator--() noexcept
+        {
+            ptr_--;
+            return *this;
+        }
+        inline const_iterator operator--(int) noexcept
+        {
+            const_iterator temp = *this;
+            --ptr_;
+            return temp;
+        }
+        inline const_iterator &operator+=(const difference_type n) noexcept
+        {
+            ptr_ += n;
+            return *this;
+        }
+        inline const_iterator &operator-=(const difference_type n) noexcept
+        {
+            ptr_ -= n;
+            return *this;
+        }
+        inline const_iterator operator+(const difference_type n) const noexcept
+        {
+            return const_iterator{ptr_ + n};
+        }
+        inline const_iterator operator-(const difference_type n) const noexcept
+        {
+            return const_iterator{ptr_ - n};
+        }
+        inline difference_type operator-(const const_iterator &other) const noexcept
+        {
+            return (ptr_ - other.ptr_);
+        }
+
+        // Comparison operators
+        inline friend bool operator==(const const_iterator &a, const const_iterator &b) noexcept
+        {
+            return (a.ptr_ == b.ptr_);
+        }
+        inline friend bool operator!=(const const_iterator &a, const const_iterator &b) noexcept
+        {
+            return (a.ptr_ != b.ptr_);
+        }
+        inline friend bool operator<(const const_iterator &a, const const_iterator &b) noexcept
+        {
+            return (a.ptr_ < b.ptr_);
+        }
+        inline friend bool operator>(const const_iterator &a, const const_iterator &b) noexcept
+        {
+            return (a.ptr_ > b.ptr_);
+        }
+        inline friend bool operator<=(const const_iterator &a, const const_iterator &b) noexcept
+        {
+            return (a.ptr_ <= b.ptr_);
+        }
+        inline friend bool operator>=(const const_iterator &a, const const_iterator &b) noexcept
+        {
+            return (a.ptr_ >= b.ptr_);
+        }
+
+        // Subscript operator
+        inline reference operator[](const difference_type n) const noexcept
+        {
+            return *(ptr_ + n);
+        }
+
+      private:
+        pointer ptr_;
+    };
+
+    using reverse_iterator = std::reverse_iterator<iterator>;
+    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+
+    inline iterator begin() noexcept
+    {
+        return iterator{static_cast<T *>(static_cast<void *>(data_))};
+    }
+
+    inline iterator end() noexcept
+    {
+        return iterator{static_cast<T *>(static_cast<void *>(data_)) + size_};
+    }
+
+    inline const_iterator cbegin() const noexcept
+    {
+        return const_iterator{static_cast<const T *>(static_cast<const void *>(data_))};
+    }
+
+    inline const_iterator cend() const noexcept
+    {
+        return const_iterator{static_cast<const T *>(static_cast<const void *>(data_)) + size_};
+    }
+
+    inline reverse_iterator rbegin() noexcept
+    {
+        return reverse_iterator{end()};
+    }
+
+    inline reverse_iterator rend() noexcept
+    {
+        return reverse_iterator{begin()};
+    }
+
+    inline const_reverse_iterator rbegin() const noexcept
+    {
+        return const_reverse_iterator{end()};
+    }
+
+    inline const_reverse_iterator rend() const noexcept
+    {
+        return const_reverse_iterator{begin()};
+    }
+
+    inline const_reverse_iterator crbegin() const noexcept
+    {
+        return const_reverse_iterator{cend()};
+    }
+
+    inline const_reverse_iterator crend() const noexcept
+    {
+        return const_reverse_iterator{cbegin()};
     }
 
     inline void push_back(const T &value)
@@ -66,7 +367,8 @@ template <typename T, std::size_t MaxSize> class StackVector final
             throw std::runtime_error("Capacity exceeded.");
         }
 
-        data_[size_++] = value;
+        new (&data_[size_ * sizeof(T)]) T{value};
+        ++size_;
     }
 
     template <typename... Args> inline void emplace_back(Args &&...args)
@@ -76,7 +378,8 @@ template <typename T, std::size_t MaxSize> class StackVector final
             throw std::runtime_error("Capacity exceeded.");
         }
 
-        new (&data_[size_++]) T{std::forward<Args>(args)...};
+        new (&data_[size_ * sizeof(T)]) T{std::forward<Args>(args)...};
+        ++size_;
     }
 
     inline void pop_back()
@@ -87,6 +390,9 @@ template <typename T, std::size_t MaxSize> class StackVector final
         }
 
         --size_;
+
+        T *element = static_cast<T *>(static_cast<void *>(&data_[size_ * sizeof(T)]));
+        element->~T();
     }
 
     void resize(const std::size_t new_size, const T &value = T{})
@@ -96,9 +402,25 @@ template <typename T, std::size_t MaxSize> class StackVector final
             throw std::runtime_error("Exceeds maximum size.");
         }
 
+        // Construct new elements if the new size is greater than the current size
         if (new_size > size_)
         {
-            std::fill(data_.begin() + size_, data_.begin() + new_size, value);
+            for (std::size_t i = size_; i < new_size; ++i)
+            {
+                new (&data_[i * sizeof(T)]) T{value};
+            }
+        }
+
+        else
+        {
+            // Destruct excess elements if the new size is smaller than the current size
+            if (new_size < size_)
+            {
+                for (std::size_t i = new_size; i < size_; ++i)
+                {
+                    static_cast<T *>(static_cast<void *>(&data_[i * sizeof(T)]))->~T();
+                }
+            }
         }
 
         size_ = new_size;
@@ -106,6 +428,12 @@ template <typename T, std::size_t MaxSize> class StackVector final
 
     inline void clear() noexcept
     {
+        // Explicitly call the destructor for each constructed element
+        for (std::size_t i = 0U; i < size_; ++i)
+        {
+            T *element = static_cast<T *>(static_cast<void *>(&data_[i * sizeof(T)]));
+            element->~T();
+        }
         size_ = 0U;
     }
 
@@ -116,7 +444,7 @@ template <typename T, std::size_t MaxSize> class StackVector final
             throw std::runtime_error("Index out of range.");
         }
 
-        return data_[index];
+        return *static_cast<T *>(static_cast<void *>(&data_[index * sizeof(T)]));
     }
 
     inline const T &at(const std::size_t index) const
@@ -126,17 +454,17 @@ template <typename T, std::size_t MaxSize> class StackVector final
             throw std::out_of_range("Index out of range.");
         }
 
-        return data_[index];
+        return *static_cast<const T *>(static_cast<const void *>(&data_[index * sizeof(T)]));
     }
 
     constexpr inline T &operator[](const std::size_t index) noexcept
     {
-        return data_[index];
+        return *static_cast<T *>(static_cast<void *>(&data_[index * sizeof(T)]));
     }
 
     constexpr inline const T &operator[](const std::size_t index) const noexcept
     {
-        return data_[index];
+        return *static_cast<const T *>(static_cast<const void *>(&data_[index * sizeof(T)]));
     }
 
     inline std::size_t size() const noexcept
@@ -154,56 +482,6 @@ template <typename T, std::size_t MaxSize> class StackVector final
         return (size_ == 0U);
     }
 
-    constexpr inline auto begin() noexcept
-    {
-        return data_.begin();
-    }
-
-    constexpr inline auto end() noexcept
-    {
-        return data_.begin() + size_;
-    }
-
-    constexpr inline auto begin() const noexcept
-    {
-        return data_.begin();
-    }
-
-    constexpr inline auto end() const noexcept
-    {
-        return data_.begin() + size_;
-    }
-
-    constexpr inline auto cbegin() noexcept
-    {
-        return data_.cbegin();
-    }
-
-    constexpr inline auto cend() noexcept
-    {
-        return data_.cend();
-    }
-
-    constexpr inline auto rbegin() noexcept
-    {
-        return data_.rbegin();
-    }
-
-    constexpr inline auto rend() noexcept
-    {
-        return data_.rend();
-    }
-
-    constexpr inline auto crbegin() noexcept
-    {
-        return data_.crbegin();
-    }
-
-    constexpr inline auto crend() noexcept
-    {
-        return data_.crend();
-    }
-
     inline T &front()
     {
         if (size_ == 0U)
@@ -211,7 +489,7 @@ template <typename T, std::size_t MaxSize> class StackVector final
             throw std::runtime_error("Empty container.");
         }
 
-        return data_.front();
+        return *static_cast<T *>(static_cast<void *>(&data_[0]));
     }
 
     inline const T &front() const
@@ -221,7 +499,7 @@ template <typename T, std::size_t MaxSize> class StackVector final
             throw std::runtime_error("Empty container.");
         }
 
-        return data_.front();
+        return *static_cast<const T *>(static_cast<const void *>(&data_[0]));
     }
 
     inline T &back()
@@ -231,7 +509,7 @@ template <typename T, std::size_t MaxSize> class StackVector final
             throw std::runtime_error("Empty container.");
         }
 
-        return data_[size_ - 1U];
+        return *static_cast<T *>(static_cast<void *>(&data_[(size_ - 1) * sizeof(T)]));
     }
 
     inline const T &back() const
@@ -241,23 +519,19 @@ template <typename T, std::size_t MaxSize> class StackVector final
             throw std::runtime_error("Empty container.");
         }
 
-        return data_[size_ - 1U];
-    }
-
-    constexpr inline T *data() noexcept
-    {
-        return data_.data();
-    }
-
-    constexpr inline const T *data() const noexcept
-    {
-        return data_.data();
+        return *static_cast<const T *>(static_cast<const void *>(&data_[(size_ - 1) * sizeof(T)]));
     }
 
   private:
     std::size_t size_;
-    std::array<T, MAX_SIZE> data_;
+    alignas(alignof(T)) std::byte data_[sizeof(T) * MAX_SIZE];
 };
+
+template <typename T, std::size_t MaxSize>
+inline void swap(StackVector<T, MaxSize> &lhs, StackVector<T, MaxSize> &rhs) noexcept
+{
+    lhs.swap(rhs);
+}
 } // namespace containers
 
 #endif // CONTAINERS_STACK_VECTOR
