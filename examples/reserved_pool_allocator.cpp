f0640e90789c0a22739114b4fe9246068bc5c5f3
Yevgeniy <yevgeniy.engineer@gmail.com>
2023-11-16 00:13:54 +0800

    Added ReservedPoolAllocator class

---
+++ examples/reserved_pool_allocator.cpp
@@ -0,0 +1,107 @@
+#include "reserved_pool_allocator.hpp"
+
+#include <cmath>
+#include <exception>
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    try
+    {
+        int stack_variable; // A variable on the stack
+
+        std::vector<int, containers::ReservedPoolAllocator<int, 40, containers::StackStorage>> stack_vector;
+
+        // Reserve space for the maximum number of elements that the allocator can handle
+        stack_vector.reserve(40);
+
+        // Check if the vector is likely on the stack
+        std::cout << "Address of stack variable: " << &stack_variable << std::endl;
+        std::cout << "Address of vector's first element: " << stack_vector.data() << std::endl;
+
+        if (std::abs(reinterpret_cast<char *>(&stack_variable) - reinterpret_cast<char *>(stack_vector.data())) <
+            1024 * 1024)
+        {
+            std::cout << "The vector is likely on the stack." << std::endl;
+        }
+        else
+        {
+            std::cout << "The vector is likely not on the stack." << std::endl;
+        }
+
+        for (int i = 0; i < 10; ++i)
+        {
+            stack_vector.push_back(i);
+        }
+
+        std::cout << "Vector capacity after adding elements: " << stack_vector.capacity() << std::endl;
+
+        for (auto &num : stack_vector)
+        {
+            std::cout << num << " ";
+        }
+        std::cout << std::endl;
+    }
+    catch (const std::exception &ex)
+    {
+        std::cerr << "Exception: " << ex.what() << std::endl;
+        return 1;
+    }
+    catch (...)
+    {
+        std::cerr << "Unknown exception!" << std::endl;
+        return 2;
+    }
+
+    std::cout << std::endl;
+
+    try
+    {
+        int stack_variable; // A variable on the stack
+
+        std::vector<int, containers::ReservedPoolAllocator<int, 40, containers::HeapStorage>> heap_vector;
+
+        // Reserve space for the maximum number of elements that the allocator can handle
+        heap_vector.reserve(40);
+
+        // Check if the vector is likely on the stack
+        std::cout << "Address of stack variable: " << &stack_variable << std::endl;
+        std::cout << "Address of vector's first element: " << heap_vector.data() << std::endl;
+
+        if (std::abs(reinterpret_cast<char *>(&stack_variable) - reinterpret_cast<char *>(heap_vector.data())) <
+            1024 * 1024)
+        {
+            std::cout << "The vector is likely on the stack." << std::endl;
+        }
+        else
+        {
+            std::cout << "The vector is likely not on the stack." << std::endl;
+        }
+
+        for (int i = 0; i < 10; ++i)
+        {
+            heap_vector.push_back(i);
+        }
+
+        std::cout << "Vector capacity after adding elements: " << heap_vector.capacity() << std::endl;
+
+        for (auto &num : heap_vector)
+        {
+            std::cout << num << " ";
+        }
+        std::cout << std::endl;
+    }
+    catch (const std::exception &ex)
+    {
+        std::cerr << "Exception: " << ex.what() << std::endl;
+        return 1;
+    }
+    catch (...)
+    {
+        std::cerr << "Unknown exception!" << std::endl;
+        return 2;
+    }
+
+    return 0;
+}
---
+++ include/reserved_pool_allocator.hpp
@@ -0,0 +1,156 @@
+#ifndef CONTAINERS_POOL_ALLOCATOR
+#define CONTAINERS_POOL_ALLOCATOR
+
+#include <cstddef>
+#include <cstdint>
+#include <cstdlib>
+#include <memory>
+#include <new>
+#include <type_traits>
+
+#include <iostream>
+
+namespace containers
+{
+struct StackPolicy
+{
+};
+
+struct HeapPolicy
+{
+};
+
+template <typename T, std::size_t MaxSize> class StackStorage
+{
+    static_assert(MaxSize > 0U, "StackStorage must contain allocated space for at least 1 element!");
+
+  public:
+    using Policy = StackPolicy;
+
+    StackStorage() = default;
+    ~StackStorage() = default;
+
+    // Delete copy and move constructors and assignment operators to prevent accidental moving of the buffer.
+    StackStorage(const StackStorage &) = delete;
+    StackStorage &operator=(const StackStorage &) = delete;
+    StackStorage(StackStorage &&) noexcept = delete;
+    StackStorage &operator=(StackStorage &&) noexcept = delete;
+
+    inline T *buffer() noexcept
+    {
+        return static_cast<T *>(static_cast<void *>(buffer_));
+    }
+
+    inline const T *buffer() const noexcept
+    {
+        return static_cast<T *>(static_cast<void *>(buffer_));
+    }
+
+  private:
+    alignas(alignof(T)) std::byte buffer_[MaxSize * sizeof(T)];
+};
+
+template <typename T, std::size_t MaxSize> class HeapStorage
+{
+    static_assert(MaxSize > 0U, "HeapStorage must contain allocated space for at least 1 element!");
+
+  public:
+    using Policy = HeapPolicy;
+
+    // Copy and move operations are deleted to prevent accidental copying
+    HeapStorage(const HeapStorage &) = delete;
+    HeapStorage &operator=(const HeapStorage &) = delete;
+    HeapStorage(HeapStorage &&) noexcept = delete;
+    HeapStorage &operator=(HeapStorage &&) noexcept = delete;
+
+    // Allocate aligned memory for MaxSize elements of type T
+    HeapStorage() : buffer_{static_cast<T *>(std::aligned_alloc(alignof(T), MaxSize * sizeof(T)))}
+    {
+        if (nullptr == buffer_)
+        {
+            throw std::bad_alloc();
+        }
+    }
+
+    ~HeapStorage()
+    {
+        // Free the allocated memory
+        std::free(buffer_);
+
+        buffer_ = nullptr;
+    }
+
+    inline T *buffer() noexcept
+    {
+        return buffer_;
+    }
+
+    inline const T *buffer() const noexcept
+    {
+        return buffer_;
+    }
+
+  private:
+    T *buffer_;
+};
+
+template <typename T, std::size_t MaxSize, template <typename, std::size_t> class StoragePolicy>
+class ReservedPoolAllocator
+{
+    static_assert(std::is_same_v<typename StoragePolicy<T, MaxSize>::Policy, StackPolicy> ||
+                      std::is_same_v<typename StoragePolicy<T, MaxSize>::Policy, HeapPolicy>,
+                  "StoragePolicy must be either StackStorage or HeapStorage");
+
+  public:
+    using size_type = std::size_t;
+    using difference_type = std::ptrdiff_t;
+    using pointer = T *;
+    using const_pointer = const T *;
+    using reference = T &;
+    using const_reference = const T &;
+    using value_type = T;
+
+    ReservedPoolAllocator() : storage_{}, used_{0U}
+    {
+    }
+
+    template <typename U> struct rebind
+    {
+        using other = ReservedPoolAllocator<U, MaxSize, StoragePolicy>;
+    };
+
+    T *allocate(const std::size_t n)
+    {
+        if ((used_ + n) > MaxSize)
+        {
+            throw std::bad_alloc();
+        }
+
+        T *result = storage_.buffer() + used_;
+        used_ += n;
+        return result;
+    }
+
+    void deallocate(T *, std::size_t)
+    {
+        // Deallocate method is a no-op for both stack and heap in this design
+    }
+
+    template <typename U> void construct(pointer p, U &&value)
+    {
+        new (p) T{std::forward<U>(value)};
+    }
+
+    void destroy(pointer p)
+    {
+        p->~T();
+    }
+
+  private:
+    StoragePolicy<T, MaxSize> storage_;
+    std::size_t used_;
+};
+
+} // namespace containers
+
+#endif // CONTAINERS_POOL_ALLOCATOR
